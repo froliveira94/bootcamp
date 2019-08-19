e3df61755e2923d6a4552de4a2649cb87b23b32f
froliveira94 <fr.oliveira94@gmail.com>
2019-08-19 18:09:28 -0300

    add: comments

--- modulo1/index.js
+++ modulo1/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const server = express();
 
+// Query Params = ?teste=1
+// Route Params = /users/1
+// Request body = { "name": "Diego", "email": "diego@rocketseat.com.br" }
+
 server.get("/users/:id", (req, res) => {
   const { id } = req.params;
   return res.json({ message: `Buscando o usuário ${id}` });
