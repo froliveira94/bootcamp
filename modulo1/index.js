238fdf759d6ae18f87e00dcd571a152126c39839
froliveira94 <fr.oliveira94@gmail.com>
2019-08-19 18:41:53 -0300

    CRUD

--- modulo1/index.js
+++ modulo1/index.js
@@ -2,15 +2,42 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 // Query Params = ?teste=1
 // Route Params = /users/1
 // Request body = { "name": "Diego", "email": "diego@rocketseat.com.br" }
 
+// CRUD - Create, Read, Update, Delete
+
 const users = ["Diego", "Robson", "Victor"];
+
+server.get("/users", (req, res) => {
+  res.json(users);
+});
 
 server.get("/users/:index", (req, res) => {
   const { index } = req.params;
   return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+  return res.send();
 });
 
 server.listen(3000);
