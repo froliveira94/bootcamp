d1f6c6733d6e725744b122d3f559e9c90ec5263f
froliveira94 <fr.oliveira94@gmail.com>
2019-08-20 16:48:12 -0300

    add: middleware to remove project

--- modulo1/desafio01/index.js
+++ modulo1/desafio01/index.js
@@ -4,7 +4,7 @@ const server = express();
 
 server.use(express.json());
 
-const projects = [{ id: "1", title: "Primeiro Projeto", tasks: [] }];
+let projects = [{ id: "1", title: "Primeiro Projeto", tasks: [] }];
 
 server.get("/projects", (req, res) => {
   return res.json(projects);
@@ -16,6 +16,13 @@ server.post("/projects", (req, res) => {
     return res.status(400).send({ error: "Title and id are required" });
   }
   projects.push({ id, title, tasks: !!tasks ? tasks : [] });
+  return res.json(projects);
+});
+
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const newProjectsArray = projects.filter(item => item.id !== id);
+  projects = newProjectsArray;
   return res.json(projects);
 });
 
