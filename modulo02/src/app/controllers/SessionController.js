722e2a7950d512fe79c5e4e999b81f910f75e127
froliveira94 <fr.oliveira94@gmail.com>
2019-08-27 09:17:51 -0300

    add: update user and token

--- modulo02/src/app/controllers/SessionController.js
+++ modulo02/src/app/controllers/SessionController.js
@@ -1,10 +1,23 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
+import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, password } = req.body;
-    const user = await user.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email } });
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
@@ -18,9 +31,11 @@ class SessionController {
         name,
         email,
       },
-      token: jwt.sign({ id }),
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
     });
   }
 }
 
-export default SessionController();
+export default new SessionController();
