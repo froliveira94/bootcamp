e29651c1d26ea14dea29481dd6c7c023fa2ad50e
froliveira94 <fr.oliveira94@gmail.com>
2019-09-03 09:25:39 -0300

    add: list appointments

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -22,6 +22,7 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
+routes.get('/appointments', AppointmentControoler.index);
 routes.post('/appointments', AppointmentControoler.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
