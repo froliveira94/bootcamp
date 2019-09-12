08a45a77c8021c3fb2e45ff03a666454ec1c2d5a
froliveira94 <fr.oliveira94@gmail.com>
2019-09-12 09:16:05 -0300

    add: email templates

--- modulo02/src/app/controllers/AppointmentController.js
+++ modulo02/src/app/controllers/AppointmentController.js
@@ -122,6 +122,11 @@ class AppointmentController {
           model: User,
           as: 'provider',
           attributes: ['name', 'email']
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name']
         }
       ]
     });
@@ -146,7 +151,16 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento'
+      template: 'cancelation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          appointment.date,
+          "'dia' dd 'de' MMMM', ás' H:mm'h'",
+          { locale: pt }
+        )
+      }
     });
 
     return res.json(appointment);
