f1a28897bac67d79d5e8ea7a153518d0df37f184
froliveira94 <fr.oliveira94@gmail.com>
2019-09-17 13:54:55 -0300

    add: sentry and envirioment variables

--- modulo02/src/config/auth.js
+++ modulo02/src/config/auth.js
@@ -1,4 +1,9 @@
+// export default {
+//   secret: 'f29618255c309de4469993cce24286ea',
+//   expiresIn: '7d',
+// };
+
 export default {
-  secret: 'f29618255c309de4469993cce24286ea',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
