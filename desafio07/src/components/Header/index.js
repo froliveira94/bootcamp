0ffac53819996495f3715617412271aa4fc40da9
Felipe Oliveira <fr.oliveira94@gmail.com>
2019-12-13 15:32:05 -0300

    fix: react native vector icons

--- desafio07/src/components/Header/index.js
+++ desafio07/src/components/Header/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Logo from './../Icons/Logo';
 
-import { Container } from './style';
+import { Container, CartContainer } from './style';
 
 export default function Header() {
   return (
     <Container>
       <Logo />
+      <CartContainer>
+        <Icon name="shopping-basket" size={30} color="#fff" />
+      </CartContainer>
     </Container>
   );
 }
--- desafio07/src/components/Header/style.js
+++ desafio07/src/components/Header/style.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.View`
-  padding: 10px 15px;
+  padding: 10px 0px;
 `;
+
+export const CartContainer = styled.TouchableOpacity``;
